da0087044ef861b9590966856cf72d7afe01cb1b
Andrew Calder <andrewcalder8@googlemail.com>
2021-10-21 18:41:14 +0100

    next function passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,7 +1,11 @@
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-def get_total_cash(cash):
-    return cash["admin"]["total_cash"]
+def get_total_cash(total_cash):
+    return total_cash["admin"]["total_cash"]
 
 
+# WILL COME BACK
+
+def get_pets_sold(get_pets):
+    return get_pets["admin"]["pets_sold"]
