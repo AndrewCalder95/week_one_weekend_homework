0b6446cf93ee9ce77d4df9285d5c60828de81558
Andrew Calder <andrewcalder8@googlemail.com>
2021-10-22 17:54:51 +0100

    last few functions passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -32,9 +32,35 @@ def get_pets_by_breed(pets, breed_search):
     return amount_of_breed
             
 def find_pet_by_name(pets,name):
-    found_pet = {"name" : "None"}
+    found_pet = {}
     for pet in pets["pets"]:
         if pet["name"] == name:
             found_pet["name"] = pet
             return found_pet["name"]
-        
+
+# def remove_pet_by_name(pets, name):
+#     for pet in pets["pets"]:
+#         if pet["name"] == name:
+#             return pet["name"] = None
+
+
+def add_pet_to_stock(pets, new_pet):
+    pets["pets"].append(new_pet)
+    return pets["pets"]
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, loss):
+    customer["cash"] -= loss
+    return customer["cash"]
+
+def get_customer_pet_count(customer):
+    customer_pets = 0
+    for individual in customer["pets"]:
+        customer_pets += 1
+    return customer_pets
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+    return customer["pets"]
